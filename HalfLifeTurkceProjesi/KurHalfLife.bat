9c5a8f6daa094e200d37a3c48ee23b161f472d98
Mehmet <mehmetaliumac@gmail.com>
2025-11-18 23:30:53 +0300

    Correct and improve dictionary translations
    Fixed typos and improved phrasing in English and Turkish dictionary files for Blue Shift and Decay CaptionMod.
    
    Changes include correcting sound token names, refining translations, and updating dialogue for accuracy and clarity.
    
    Also removed obsolete and added new plugin configuration files.

--- HalfLifeTurkceProjesi/KurHalfLife.bat
+++ HalfLifeTurkceProjesi/KurHalfLife.bat
@@ -1,46 +1,158 @@
 @echo off
 
-if not exist "%~dp0Build\svencoop.exe" goto fail_nobuild
-
 set LauncherExe=metahook.exe
 set LauncherMod=valve
 set FullGameName=Half-Life
 set ShortGameName=HalfLife
+set GameAppId=70
 
-for /f "delims=" %%a in ('"%~dp0SteamAppsLocation/SteamAppsLocation" 70 InstallDir') do set GameDir=%%a
+:: Check if SolutionDir is already set and non-empty
+if not defined SolutionDir (
+    :: Only set SolutionDir if it's not already set
+    SET "SolutionDir=%~dp0.."
+)
 
-if "%GameDir%"=="" goto fail
+:: Ensure the path ends with a backslash
+if not "%SolutionDir:~-1%"=="\" SET "SolutionDir=%SolutionDir%\"
 
-echo -----------------------------------------------------
+if exist "%SolutionDir%Build\MetaHook.exe" goto start_install
+if exist "%SolutionDir%Build\MetaHook_blob.exe" goto start_install
 
-echo Dosyalar kopyalaniyor...
-
-copy "%~dp0Build\svencoop.exe" "%GameDir%\%LauncherExe%" /y
-xcopy "%~dp0Build\valve" "%GameDir%\%LauncherMod%" /y /e
-
-if not exist "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst" copy "%GameDir%\%LauncherMod%\metahook\configs\plugins_goldsrc.lst" "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst" /y
-
-del "%GameDir%\%LauncherMod%\metahook\configs\plugins_goldsrc.lst"
-del "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop.lst"
-del "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop_avx2.lst"
-
-powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut([System.IO.Path]::Combine($Env:USERPROFILE, 'Desktop', 'Altyazili %ShortGameName%.lnk'));$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -game %LauncherMod%\";$shortcut.Save();
-
-echo -----------------------------------------------------
-
-echo Bitti.
-echo Lutfen oyunu "Altyazili %ShortGameName%" dosyasindan calistir.
+echo Error: Compiled binaries not found ! You have to download compiled zip from github release page or compile the sources by yourself before installing !!!
 pause
 exit
 
-:fail
+:start_install
 
-echo %FullGameName% oyunu için dizin bulunamadi, lutfen Steam'in calisir oldugundan ve %FullGameName% oyununu kurdugundan emin ol.
+if not "%GameDir%"=="" (
+
+    if not exist "%GameDir%\" (
+        echo Error: The GameDir "%GameDir%" is not existing !!!
+        pause
+        exit
+    )
+
+    if not exist "%GameDir%\%LauncherMod%" (
+        echo Error: The ModDir "%GameDir%\%LauncherMod%" is not existing !!!
+        pause
+        exit
+    )
+
+    if not exist "%GameDir%\%LauncherMod%\liblist.gam" (
+        echo Error: The ModDir "%GameDir%\%LauncherMod%" is not a valid Mod !!!
+        pause
+        exit
+    )
+
+    goto start_copy
+)
+
+echo %GameAppId% > "%SolutionDir%tools\steam_appid.txt"
+
+for /f "delims=" %%a in ('"%SolutionDir%\tools\SteamAppsLocation" %GameAppId% InstallDir') do set OutputString=%%a
+
+if not "%OutputString%"=="" (
+    set "GameDir=%OutputString%"
+    goto start_copy
+)
+
+echo Error: Failed to locate GameInstallDir of %FullGameName%, please make sure Steam is running and you have %FullGameName% installed correctly.
 pause
 exit
 
-:fail_nobuild
+:start_copy
 
-echo Derlenmis dosyalar bulunamadi!
+echo -----------------------------------------------------
+
+echo Copying files...
+
+if exist "%SolutionDir%Build\MetaHook.exe" copy "%SolutionDir%Build\MetaHook.exe" "%GameDir%\%LauncherExe%" /y
+if exist "%SolutionDir%Build\MetaHook_blob.exe" copy "%SolutionDir%Build\MetaHook_blob.exe" "%GameDir%\%LauncherExe%" /y
+
+mkdir "%GameDir%\%LauncherMod%\"
+xcopy "%SolutionDir%Build\svencoop" "%GameDir%\%LauncherMod%" /y /e
+
+if "%LauncherMod%"=="svencoop" (
+
+    mkdir "%GameDir%\%LauncherMod%_downloads\"
+    xcopy "%SolutionDir%Build\svencoop_downloads" "%GameDir%\%LauncherMod%_downloads\" /y /e
+
+    mkdir "%GameDir%\%LauncherMod%_schinese\"
+    xcopy "%SolutionDir%Build\svencoop_schinese" "%GameDir%\%LauncherMod%_schinese\" /y /e
+
+    mkdir "%GameDir%\platform\"
+    xcopy "%SolutionDir%Build\platform" "%GameDir%\platform" /y /e
+    
+    mkdir "%GameDir%\%LauncherMod%_hidpi\"
+    xcopy "%SolutionDir%Build\svencoop_hidpi" "%GameDir%\%LauncherMod%_hidpi\" /y /e
+
+    if not exist "%GameDir%\%LauncherMod%\metahook\configs\dllpaths.lst" copy "%GameDir%\%LauncherMod%\metahook\configs\dllpaths.lst" "%GameDir%\%LauncherMod%\metahook\configs\dllpaths.lst" /-y
+    if not exist "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst" copy "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop.lst" "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst" /-y
+
+) else (
+
+        mkdir "%GameDir%\%LauncherMod%_hidpi\"
+        xcopy "%SolutionDir%Build\svencoop_hidpi" "%GameDir%\%LauncherMod%_hidpi\" /y /e
+
+
+	if "%LauncherMod%"=="cstrike" (
+
+		if exist "%GameDir%\cstrike_hd" (
+
+			xcopy "%SolutionDir%Build\%LauncherMod%_hd" "%GameDir%\%LauncherMod%_hd\" /y /e
+
+		)
+
+		xcopy "%SolutionDir%Build\cstrike" "%GameDir%\%LauncherMod%\" /y /e
+	)
+
+	if "%LauncherMod%"=="czero" (
+
+		xcopy "%SolutionDir%Build\czero" "%GameDir%\%LauncherMod%\" /y /e
+	)
+
+	if "%LauncherMod%"=="echoes" (
+
+		xcopy "%SolutionDir%Build\echoes" "%GameDir%\%LauncherMod%\" /y /e
+	)
+
+	if "%LauncherMod%"=="gearbox" (
+
+		xcopy "%SolutionDir%Build\gearbox" "%GameDir%\%LauncherMod%\" /y /e
+	)
+
+    if not exist "%GameDir%\%LauncherMod%\metahook\configs\dllpaths.lst" copy "%GameDir%\%LauncherMod%\metahook\configs\dllpaths.lst" "%GameDir%\%LauncherMod%\metahook\configs\dllpaths.lst" /-y
+    if not exist "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst" copy "%GameDir%\%LauncherMod%\metahook\configs\plugins_goldsrc.lst" "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst" /-y
+)
+
+echo -------------------------------------------------------
+echo Cleaning deprecated files from previous version of MetaHookSv...
+
+if exist "%GameDir%\%LauncherMod%\metahook\configs\plugins_goldsrc.lst" del "%GameDir%\%LauncherMod%\metahook\configs\plugins_goldsrc.lst"
+if exist "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop.lst" del "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop.lst"
+if exist "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop_avx2.lst" del "%GameDir%\%LauncherMod%\metahook\configs\plugins_svencoop_avx2.lst"
+if exist "%GameDir%\FreeImage.dll" del "%GameDir%\FreeImage.dll"
+if exist "%GameDir%\%LauncherMod%_hidpi\resource\CreateMultiplayerGameDialog.res" del "%GameDir%\%LauncherMod%_hidpi\resource\CreateMultiplayerGameDialog.res"
+if exist "%GameDir%\%LauncherMod%_hidpi\resource\OptionsDialog.res" del "%GameDir%\%LauncherMod%_hidpi\resource\OptionsDialog.res"
+
+echo -----------------------------------------------------
+echo Make sure that all plugins you want has been added into the plugins.lst
+
+notepad "%GameDir%\%LauncherMod%\metahook\configs\plugins.lst"
+
+echo -----------------------------------------------------
+echo Done
+
+
+cd /d "%SolutionDir%"
+
+powershell $shell = New-Object -ComObject WScript.Shell;$shortcut = $shell.CreateShortcut(\"MetaHook for %ShortGameName%.lnk\");$shortcut.TargetPath = \"%GameDir%\%LauncherExe%\";$shortcut.WorkingDirectory = \"%GameDir%\";$shortcut.Arguments = \"-insecure -game %LauncherMod%\";$shortcut.Save();
+
+echo -----------------------------------------------------
+echo Please launch game from shortcut "MetaHook for %ShortGameName%"
+
+start explorer "%SolutionDir%"
+
 pause
+
 exit
